c66c0b30cd0e0e8c6e6d317333884844c9ac0115
BIT Student <classroom@bgit.rs>
2019-03-29 14:58:35 +0100

    short-email

--- react-lf/src/entities/userObj.js
+++ react-lf/src/entities/userObj.js
@@ -1,8 +1,9 @@
+
 class UserObj {
     constructor(gender, name, email, dobDate, picture, loginName) {
         this.gender = gender;
         this.name = name.first.charAt(0).toUpperCase() + name.first.slice(1) + " " + name.last.charAt(0).toUpperCase() + name.last.slice(1);
-        this.email = email;
+        this.email = email.slice(0, 3) + '...' + email.slice(email.indexOf('@') - 3, email.indexOf('@')) + email.slice(email.indexOf('@'));
         this.dobDate = new Date(dobDate).toDateString();
         this.picture = picture;
         this.loginName = loginName;
