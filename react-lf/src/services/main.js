5eb07b45223aa04b67c9f0da4a2d3873a112da5c
BIT Student <classroom@bgit.rs>
2019-03-29 17:28:39 +0100

    grid

--- react-lf/src/services/main.js
+++ react-lf/src/services/main.js
@@ -6,6 +6,7 @@ import React from 'react';
 
 
 
+
 const Main = (props) => (
 
     props.users.map(user => (
@@ -27,44 +28,31 @@ const Main = (props) => (
     )
     )
 )
-// < div >
-// <p>{user.name}</p>
-// <p>{user.email}</p>
-// <p>{user.dobDate}</p>
+
+export default Main;
+
+
+// const Main = (props) => (
+
+//     props.users.map(user => (
+
+
+//         < div className={`${user.gender} users`} key={user.loginName} >
+
+//             {/* <div className="col s12 m6"> */}
+//             <div class="card blue-grey darken-1">
+//                 <div className="card-content white-text">
+//                     <img className='avatar' src={user.picture} />
+//                     <span className="card-title">{user.name}</span>
+//                     <p> {user.email}</p>
+//                     <p>{user.dobDate}</p>
+//                     {/* <p></p> */}
+//                 </div>
+//             </div>
+//             {/* </div> */}
+
 //         </div >
 
-// < div className = {`${user.gender} users`} key = { user.loginName } >
-//     < div className="row" >
-//         {/* <div className="col s12 m6"> */}
-//         <div class="card blue-grey darken-1">
-//             <div className="card-content white-text">
-// <img className='avatar' src={user.picture2} />
-//                 <span className="card-title">{user.name}</span>
-// < p > { user.email }</p>
-//     <p>{user.dobDate}</p>
-//                 {/* <p></p> */}
-//             </div>
-//         </div>
-//         {/* </div> */}
-//     </div >
-//     </div >
-
-
-{/* <div class="row">
-<div class="col s12 m7">
-  <div class="card">
-    <div class="card-image">
-      <img src="images/sample-1.jpg">
-      <span class="card-title">Card Title</span>
-    </div>
-    <div class="card-content">
-      <p>I am a very simple card. I am good at containing small bits of information.
-      I am convenient because I require little markup to use effectively.</p>
-    </div>
-    <div class="card-action">
-      <a href="#">This is a link</a>
-    </div>
-  </div>
-</div>
-</div> */}
-export default Main;
+//     )
+//     )
+// )
