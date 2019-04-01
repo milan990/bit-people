73b7fd54f7ed723bc2d458ba29c4e98feb5e3338
BIT Student <classroom@bgit.rs>
2019-04-01 14:24:08 +0200

    feature-4

--- react-lf/src/components/header.js
+++ react-lf/src/components/header.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const Header = (props) => (
     <header>
         <h2>React Users</h2>
-        <button onClick={props.event}><i class="fa fa-th-large"></i> Grid</button>
+        <button onClick={props.event}><i class="fa fa-th-large"></i>List/Grid</button>
     </header>
 )
